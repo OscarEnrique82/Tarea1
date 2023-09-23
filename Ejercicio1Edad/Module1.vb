0fcd4ad0f8fb61275caed1f14f8bce66f9b3e890
Oscar Mejia <dsnp011423@ugb.edu.sv>
2023-09-23 10:57:00 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1Edad/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+    ' Oscar Enrique Mejía Chávez DSNP011423
+    Sub Main()
+        ' Declaración de variables
+        Dim Edad As Integer
+        ' Toma de datos
+        While True
+            Console.WriteLine("------------------------------")
+            Console.WriteLine("Ingrese la edad de la persona:")
+            Console.WriteLine("------------------------------")
+            Edad = Console.ReadLine()
+
+            ' Evalaución de datos
+            If (Edad >= 18) Then
+                ' Mostrar en pantalla el resultado
+                Console.WriteLine("------------------------------------------------------")
+                Console.WriteLine("La persona tiene " & Edad & " años por lo tanto es mayor de edad")
+                Console.WriteLine("------------------------------------------------------")
+                Console.WriteLine()
+            Else
+                ' Mostrar en pantalla el resultado
+                Console.WriteLine("------------------------------------------------------")
+                Console.WriteLine("La persona tiene " & Edad & " años por lo tanto es menor de edad")
+                Console.WriteLine("------------------------------------------------------")
+                Console.WriteLine()
+            End If
+            Dim resp As String
+            Console.WriteLine("----------------------------------------------------------------------")
+            Console.WriteLine("Presione cualquier tecla para comparar otra edad o digite s para salir")
+            Console.WriteLine("----------------------------------------------------------------------")
+            resp = Console.ReadLine()
+            If resp = "s" Or resp = "S" Then
+                Exit While
+            End If
+        End While
+    End Sub
+
+End Module
